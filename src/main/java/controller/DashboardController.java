9ee9e5bbfdb76213fcb8199117ae63ddbc1e1100
Ho Quang Thanh <thanhhqce181480@fpt.edu.vn>
2025-06-18 20:04:20 +0700

    add hard code to redirect to dashboard servlet for demo iter1 (not: username = admin and password = 123456)

---
+++ src/main/java/controller/DashboardController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        name = "DashboardController",
+        value = "/dashboard"
+)
+public class DashboardController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("hello");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
